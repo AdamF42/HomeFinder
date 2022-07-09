ccf51e08f0d5f98849f0692ab794008579f896fb
Adamo <adamo.fapohunda@studio.unibo.it>
2022-07-09 23:32:25 +0200

    Added more websites

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -3,6 +3,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import pages.*;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -11,13 +12,21 @@ class Bot extends TelegramLongPollingBot {
 
     RunnableImpl imm;
     RunnableImpl sub;
+    RunnableImpl idea;
+    RunnableImpl cs;
 
     // private static final long millis = 5*60*1000;
     private static final long millis = 10 * 1000;
-    private static final String immUrl = "https://www.immobiliare.it/affitto-case/bologna/?criterio=rilevanza&prezzoMinimo=200&prezzoMassimo=900&superficieMinima=60&localiMinimo=3&idMZona[]=17&idMZona[]=23&idMZona[]=31";
-    private static final String subitoUrl = "https://www.subito.it/annunci-emilia-romagna/affitto/appartamenti/bologna/bologna/?ps=300&pe=800&rs=3";
+    private static final int maxPrice = 900;
+    private static final int minPrice = 200;
+    private static final int roomNumber = 3;
 
-    static ExecutorService executor = Executors.newFixedThreadPool(2);
+    private static final String immUrl = "https://www.immobiliare.it/affitto-case/bologna/?criterio=dataModifica&ordine=desc&prezzoMinimo=" + minPrice + "&prezzoMassimo=" + maxPrice + "&superficieMinima=60&localiMinimo=" + roomNumber + "&idMZona[]=17&idMZona[]=23&idMZona[]=31";
+    private static final String subitoUrl = "https://www.subito.it/annunci-emilia-romagna/affitto/appartamenti/bologna/bologna/?ps=" + minPrice + "&pe=" + maxPrice + "&rs=" + roomNumber;
+    private static final String idealistaUrl = "https://www.idealista.it/affitto-case/bologna-bologna/con-prezzo_" + maxPrice + ",prezzo-min_" + minPrice + ",trilocali-3,quadrilocali-4,5-locali-o-piu/?ordine=pubblicazione-desc";
+    private static final String casaUrl = "https://www.casa.it/srp/?tr=affitti&numRoomsMin=" + roomNumber + "&priceMin=" + minPrice + "&priceMax=" + maxPrice + "&propertyTypeGroup=case&q=12d65861%2C171c3cab%2C035cb1f0%2C853e62a4%2Cd2a360ef%2Cd80cd525";
+
+    static ExecutorService executor = Executors.newFixedThreadPool(4);
 
     @Override
     public String getBotUsername() {
@@ -26,30 +35,44 @@ class Bot extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        return "";
+        return "565399062:AAHd-_CapBje_0Bq6sa_CGoLOYC8RcFDJVM";
     }
 
     public void onUpdateReceived(Update update) {
 
         Page immobiliare = new Immobiliare(immUrl);
         Page subito = new Subito(subitoUrl);
+        Page idealista = new Idealista(idealistaUrl);
+        Page casa = new Casa(casaUrl);
 
         String chatId = update.getMessage().getChatId().toString();
         switch (update.getMessage().getText()) {
-            case "start" -> {
-                imm = RunnableImpl.getInstance(this, chatId, immobiliare, millis);
+            case "start":
+                imm = new RunnableImpl(this, chatId, immobiliare, millis);
                 imm.setShouldRun(true);
                 executor.submit(imm);
+
                 sub = new RunnableImpl(this, chatId, subito, millis);
                 sub.setShouldRun(true);
                 executor.submit(sub);
-            }
-            case "stop" -> {
+
+                idea = new RunnableImpl(this, chatId, idealista, millis);
+                idea.setShouldRun(true);
+                executor.submit(idea);
+
+                cs = new RunnableImpl(this, chatId, casa, millis);
+                cs.setShouldRun(true);
+                executor.submit(cs);
+
+                break;
+            case "stop":
                 imm.setShouldRun(false);
                 sub.setShouldRun(false);
-            }
-            default -> {
-            }
+                idea.setShouldRun(false);
+                cs.setShouldRun(false);
+                break;
+            default:
+
         }
     }
 
---
+++ src/main/java/pages/Idealista.java
@@ -0,0 +1,81 @@
+package pages;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Idealista implements Page{
+
+    private Document document;
+    private String startUrl;
+    private static final String baseUrl = "https://www.idealista.it";
+
+    public Idealista(String url) {
+        document = getDocument(url);
+        startUrl = url;
+    }
+
+    @Override
+    public List<String> getLinks() {
+        return getLinks(document);
+    }
+
+    @Override
+    public boolean hasNextPage() {
+        return document.select("#main-content > section > div > ul > li.next > a").stream().findFirst().isPresent();
+    }
+
+    @Override
+    public Page getNextPage() {
+        Elements elements = document.select("#main-content > section > div > ul > li.next > a");
+        String url = elements.stream()
+                .map(e -> e.attributes().get("href")) //
+                .findFirst().get();
+        return new Idealista(baseUrl + url);
+    }
+
+    @Override
+    public String getStartUrl() {
+        return startUrl;
+    }
+
+    private static Document getDocument(String url) {
+        Connection conn = Jsoup.connect(url)
+                .header("Host", "www.idealista.it")
+                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+                .header("Accept-Language", "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3")
+                .header("Accept-Encoding", "gzip, deflate, br")
+                .header("Connection", "keep-alive")
+                .header("DNT", "1")
+                .header("upgrade-insecure-requests", "1")
+                .header("sec-fetch-site", "none")
+                .header("sec-fetch-mode", "navigate")
+                .timeout(5000)
+                .referrer("http://www.google.com")
+                .userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36");
+        Document document = null;
+        try {
+            document = conn.get();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // handle error
+        }
+        return document;
+    }
+
+    private static List<String> getLinks(Document document) {
+        return document.select("#main-content > section > .item > .item-info-container > a").stream()
+                .map(e -> baseUrl + e.attributes().get("href")) //
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Idealista clone() {
+        return new Idealista(document.location());
+    }
+}
